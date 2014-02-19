dc4935e788c3f16181e36ac593f1f27ba89f3fb3
Andrey Yun <andrey_yun@epam.com>
2014-02-19 21:04:19 +0600

    Runner changed

--- src/main/java/com/epam/users/Runner.java
+++ src/main/java/com/epam/users/Runner.java
@@ -3,7 +3,8 @@ package com.epam.users;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by Andrey Yun on 16.02.14.
@@ -17,8 +18,14 @@ public class Runner {
         query.setParameter("role_name","admin");
         Role role = (Role) query.getSingleResult();
         User user = new User();
-        user.setName("Ivan");
-        user.setDateOfBirth(new Date(20019991003L));
+        user.setFirstName("Ivan");
+        user.setLastName("Ivanov");
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.YEAR,1988);
+        calendar.set(Calendar.MONTH,3);
+        calendar.set(Calendar.DAY_OF_MONTH,10);
+        java.util.Date date = calendar.getTime();
+        user.setDateOfBirth(date);
         user.setPassword("123");
         user.setTelephone("5556688");
         user.getRoles().add(role);

01b7067dc9444bb98b842ba36e137cc30baf5b34
Andrey Yun <andrey_yun@epam.com>
2014-02-19 20:42:40 +0600

    updated user fields

--- src/main/java/com/epam/users/User.java
+++ src/main/java/com/epam/users/User.java
@@ -12,22 +12,37 @@ import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@NamedQueries(
+        {@NamedQuery(name = User.FIND_BY_USER_PASS, query = "from User u where u.login = :login and u.password = :pass")}
+)
 public class User implements Serializable {
     private int id;
-    private String name;
+    private String firstName;
+    private String middleName;
+    private String lastName;
     private Date dateOfBirth;
     private String telephone;
+    private String email;
+    private String login;
     private String password;
     private Set<Role> roles = new HashSet<Role>();
+
+    public static final String FIND_BY_USER_PASS = "login";
 
     public User() {
     }
 
-    public User(int id, String name, Date dateOfBirth, String telephone, String password, Set<Role> roles) {
+    public User(int id, String firstName, String middleName, String lastName,
+                Date dateOfBirth, String telephone, String email,
+                String login, String password, Set<Role> roles) {
         this.id = id;
-        this.name = name;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
         this.telephone = telephone;
+        this.email = email;
+        this.login = login;
         this.password = password;
         this.roles = roles;
     }
@@ -42,16 +57,32 @@ public class User implements Serializable {
         this.id = id;
     }
 
-    @Column(name = "name", nullable = false)
-    public String getName() {
-        return name;
+    @Column(name = "first_name", nullable = false)
+    public String getFirstName() {
+        return firstName;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "middle_name")
+    public String getMiddleName() {
+        return middleName;
+    }
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    @Column(name = "last_name", nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "dob")
+    @Column(name = "dob", nullable = false)
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
@@ -65,6 +96,22 @@ public class User implements Serializable {
     }
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    @Column(name = "login", nullable = false)
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column(name = "email", nullable = false)
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Column(name = "pass")
@@ -82,7 +129,6 @@ public class User implements Serializable {
     public Set<Role> getRoles() {
         return roles;
     }
-
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
